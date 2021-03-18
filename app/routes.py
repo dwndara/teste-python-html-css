8d7d75e3cc464622ebb81d457c48f31467e9d227
dwndara <dandaracz@gmail.com>
2021-03-18 20:17:18 +0000

    microblog

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Dandara'}
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
