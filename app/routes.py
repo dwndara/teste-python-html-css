d03b928791af546718c049e3f0ca71f4ad06f317
dwndara <dandaracz@gmail.com>
2021-03-18 22:00:48 +0000

    microblog

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Dandara'}
+    user = {'username': 'Feulo'}
     posts = [
-        {'author': {'username': 'Jully'}, 'body': "Gameplays??"},
-        {'author': {'username': 'Luiz'}, 'body': "Vavazada??"},
-        {'author': {'username': 'Fábio'}, 'body': "Rocketzada!!!"},
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
     ]
     return render_template("index.html", user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html" , title="Login")
