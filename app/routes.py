7c2bde62e2ea6e4c2c4139909e809c0986e45dd3
dwndara <dandaracz@gmail.com>
2021-03-18 20:37:40 +0000

    microblog

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from flask import render_template
 def index():
     user = {'username': 'Dandara'}
     posts = [
-        {'author': {'username': 'Jully'}, 'body': "Gaymeplays"},
+        {'author': {'username': 'Jully'}, 'body': "Gameplaysgit add"},
         {'author': {'username': 'Luiz'}, 'body': "Vavazada??"},
         {'author': {'username': 'Fábio'}, 'body': "Rocketzada!!!"},
     ]
